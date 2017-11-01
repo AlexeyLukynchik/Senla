7ace95bb4e614cc80dda59eb10115ae0319302c9
DESKTOP-3EGDEV1\Алексей <aleksejlukynchik@gmail.com>
2017-11-01 15:44:07 +0300

    delete old

--- HW_4/2/run.bat
+++
@@ -1,2 +0,0 @@
-set CLASSPATH=D:\jar\textFileWorker.jar 
-java -jar task4.jar
--- HW_4/3/start.bat
+++
@@ -1,2 +0,0 @@
-set CLASSPATH=D:\jar\0\textFileWorker.jar 
-java -jar task4.jar D:/labj/task4/src/com/company/reposRoom.txt D:/labj/task4/src/com/company/reposClient.txt D:/labj/task4/src/com/company/reposService.txt D:/labj/task4/src/com/company/reposHistory.txt 
